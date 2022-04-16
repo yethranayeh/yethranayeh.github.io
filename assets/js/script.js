ab777498c443919b63e6dbd624516151444181d4
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2022-04-16 17:26:38 +0300

    added dynamic year changes to copyright text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,35 @@
+/** @format */
+
 // Keep dropdown open for smaller screens
-const projectsDropdown = document.getElementById("projects-dropdown")
-const projectsDropdownToggle = document.getElementById("projects-dropdown-toggle")
-const projectsDropdownMenu = document.getElementById("projects-dropdown-menu")
+const projectsDropdown = document.getElementById("projects-dropdown");
+const projectsDropdownToggle = document.getElementById("projects-dropdown-toggle");
+const projectsDropdownMenu = document.getElementById("projects-dropdown-menu");
 
+window.addEventListener("resize", function (event) {
+	check_window_size();
+});
 
-window.addEventListener('resize', function(event){check_window_size()});
-
-function check_window_size(){
-    if (screen.width <992) {
-        
-        if (!projectsDropdown.classList.contains("show")) {
-            projectsDropdown.classList.add("show")
-            projectsDropdownMenu.classList.add("show")
-            projectsDropdownMenu.style = "margin-left: 0"
-            projectsDropdownToggle.setAttribute("aria-expanded", "true")
-        }
-    } else {
-        if (projectsDropdown.classList.contains("show")) {
-            projectsDropdown.classList.remove("show")
-            projectsDropdownMenu.classList.remove("show")
-            projectsDropdownMenu.style = "margin-left: -200px"
-            projectsDropdownToggle.setAttribute("aria-expanded", "false")
-        }
-    }
+function check_window_size() {
+	if (screen.width < 992) {
+		if (!projectsDropdown.classList.contains("show")) {
+			projectsDropdown.classList.add("show");
+			projectsDropdownMenu.classList.add("show");
+			projectsDropdownMenu.style = "margin-left: 0";
+			projectsDropdownToggle.setAttribute("aria-expanded", "true");
+		}
+	} else {
+		if (projectsDropdown.classList.contains("show")) {
+			projectsDropdown.classList.remove("show");
+			projectsDropdownMenu.classList.remove("show");
+			projectsDropdownMenu.style = "margin-left: -200px";
+			projectsDropdownToggle.setAttribute("aria-expanded", "false");
+		}
+	}
 }
 check_window_size();
 
 // Dynamic Age update
-document.querySelector("#my_age").textContent = new Date().getFullYear() - 1999
+const year = new Date().getFullYear();
+document.querySelector("#my_age").textContent = year - 1999;
+
+document.querySelector("#copyright-year").textContent = year;
