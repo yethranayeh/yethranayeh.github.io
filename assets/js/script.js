d425f57b5af401032786bcb7bfd5e6578ca392f9
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2022-07-01 13:46:05 +0300

    updated script to wait for DOM Load before adding my age

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,13 @@
 /** @format */
 
-// Keep dropdown open for smaller screens
-const projectsDropdown = document.getElementById("projects-dropdown");
-const projectsDropdownToggle = document.getElementById("projects-dropdown-toggle");
-const projectsDropdownMenu = document.getElementById("projects-dropdown-menu");
-
-window.addEventListener("resize", function (event) {
-	check_window_size();
-});
-
-function check_window_size() {
-	if (screen.width < 992) {
-		if (!projectsDropdown.classList.contains("show")) {
-			projectsDropdown.classList.add("show");
-			projectsDropdownMenu.classList.add("show");
-			projectsDropdownMenu.style = "margin-left: 0";
-			projectsDropdownToggle.setAttribute("aria-expanded", "true");
-		}
-	} else {
-		if (projectsDropdown.classList.contains("show")) {
-			projectsDropdown.classList.remove("show");
-			projectsDropdownMenu.classList.remove("show");
-			projectsDropdownMenu.style = "margin-left: -200px";
-			projectsDropdownToggle.setAttribute("aria-expanded", "false");
-		}
-	}
-}
-check_window_size();
-
 // Dynamic year update
-const year = new Date().getFullYear();
-document.querySelector("#my_age").textContent = year - 1999;
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		const year = new Date().getFullYear();
+		document.querySelector("#my_age").textContent = year - 1999;
 
-document.querySelector("#copyright-year").textContent = year;
+		document.querySelector("#copyright-year").textContent = year;
+	},
+	false
+);
