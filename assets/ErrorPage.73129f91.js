0560fbac2ff730e7339e2c01aaaf94407f60068c
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2023-12-13 22:13:08 +0300

    update dist files

--- assets/ErrorPage.73129f91.js
+++
@@ -1,1 +0,0 @@
-import{M as s}from"./MailLink.85b6a150.js";import{u as n,a as t,j as a,F as l,b as r,T as c}from"./index.77b74374.js";function u(){const{t:o}=n("content"),e=t();return console.error(e),a(l,{direction:"column",style:{color:"#fff"},gap:8,children:[r(c,{children:o("error.fallback-element")}),r(s,{})]})}export{u as ErrorPage};
