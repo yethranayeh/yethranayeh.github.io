a7f0344c2ad7913d126b607534983008800269be
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2023-12-16 14:13:35 +0300

    update dist files

---
+++ assets/ErrorPage.cd99b786.js
@@ -0,0 +1,1 @@
+import{M as s}from"./MailLink.732a94d1.js";import{u as n,a as t,j as a,F as l,b as r,T as c}from"./index.a40e48cb.js";function u(){const{t:o}=n("content"),e=t();return console.error(e),a(l,{direction:"column",style:{color:"#fff"},gap:8,children:[r(c,{children:o("error.fallback-element")}),r(s,{})]})}export{u as ErrorPage};
---
+++ assets/MailLink.732a94d1.js
@@ -0,0 +1,1 @@
+import{b as a,A as o}from"./index.a40e48cb.js";const c=()=>a(o,{href:"mailto:contact@aktasalper.com",children:"contact@aktasalper.com"});export{c as M};
