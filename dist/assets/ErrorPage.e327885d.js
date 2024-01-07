3be0a014b1cf487000a24875aadfe976affd85d9
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2024-01-07 23:55:56 +0300

    update dist files

---
+++ dist/assets/ErrorPage.e327885d.js
@@ -0,0 +1,1 @@
+import{M as s}from"./MailLink.02a496f8.js";import{u as n,a as t,j as a,F as l,b as r,T as c}from"./index.df803a23.js";function u(){const{t:o}=n("content"),e=t();return console.error(e),a(l,{direction:"column",style:{color:"#fff"},gap:8,children:[r(c,{children:o("error.fallback-element")}),r(s,{})]})}export{u as ErrorPage};
