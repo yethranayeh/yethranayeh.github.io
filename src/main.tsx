4e131f62c3d57f208a29e80a9758021a1b1ebd54
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2024-06-14 12:37:30 +0300

    Revert "military intervention"
    This reverts commit a9cce5daace54f279c1c30671c10cbe204d91597.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import "./styles/reset.css";
 
 import "@/i18n/init_i18n";
-import { Tally } from "./Tally";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<Tally />
+		<App />
 	</React.StrictMode>
 );
