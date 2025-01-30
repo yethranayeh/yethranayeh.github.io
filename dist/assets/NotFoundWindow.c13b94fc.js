72f8dea6f7fd98932587119124d9fc9a99b03265
Alper Aktaş <aktasalper99@gmail.com>
2025-01-30 15:45:05 +0300

    update dist files

---
+++ dist/assets/NotFoundWindow.c13b94fc.js
@@ -0,0 +1,1 @@
+import{u as e,D as a,b as n,W as s}from"./index.40637e2b.js";import{D as i,w as r}from"./DraggableWindow.161844aa.js";function d(){const{t}=e("content"),o=a().pathname.replace("/","");return n(i,{title:t("404.title"),children:n(s,{className:r.content,children:t("404.descripton",{path:o})})})}export{d as NotFoundWindow};
