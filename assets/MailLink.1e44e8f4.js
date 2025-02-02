9ea8488d72df8b8e9b1bed1d7ac853d80b29f14d
Alper Aktaş <aktasalper99@gmail.com>
2025-02-02 22:33:42 +0300

    update dist

--- assets/MailLink.1e44e8f4.js
+++
@@ -1,1 +0,0 @@
-import{b as a,A as o}from"./index.f343c0d0.js";const c=()=>a(o,{href:"mailto:contact@aktasalper.com",children:"contact@aktasalper.com"});export{c as M};
---
+++ assets/MailLink.c0fa4161.js
@@ -0,0 +1,1 @@
+import{b as a,A as o}from"./index.7cb8c075.js";const c=()=>a(o,{href:"mailto:contact@aktasalper.com",children:"contact@aktasalper.com"});export{c as M};
