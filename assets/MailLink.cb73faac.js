7bf7717a480768920d6b417b3959636b140b8fa0
Alper Aktaş <aktasalper99@gmail.com>
2025-02-07 00:09:37 +0300

    update dist

---
+++ assets/MailLink.cb73faac.js
@@ -0,0 +1,1 @@
+import{b as a,A as o}from"./index.bf253f1c.js";const c=()=>a(o,{href:"mailto:contact@aktasalper.com",children:"contact@aktasalper.com"});export{c as M};
---
+++ assets/NotFoundWindow.f478437d.js
@@ -0,0 +1,1 @@
+import{u as e,D as a,b as n,W as s}from"./index.bf253f1c.js";import{D as i,w as r}from"./DraggableWindow.cb1cc246.js";function d(){const{t}=e("content"),o=a().pathname.replace("/","");return n(i,{title:t("404.title"),children:n(s,{className:r.content,children:t("404.descripton",{path:o})})})}export{d as NotFoundWindow};
