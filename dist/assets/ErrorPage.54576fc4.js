9fda2d8d2b4b119929c0fe6dcabe10257bfd8a27
Alper Aktaş <aktasalper99@gmail.com>
2025-06-14 17:07:05 +0300

    dist files

--- dist/assets/ErrorPage.54576fc4.js
+++
@@ -1,1 +0,0 @@
-import{M as s}from"./MailLink.cb73faac.js";import{u as n,a as t,j as a,F as l,b as r,T as c}from"./index.bf253f1c.js";function u(){const{t:o}=n("content"),e=t();return console.error(e),a(l,{direction:"column",style:{color:"#fff"},gap:8,children:[r(c,{children:o("error.fallback-element")}),r(s,{})]})}export{u as ErrorPage};
--- dist/assets/MailLink.cb73faac.js
+++
@@ -1,1 +0,0 @@
-import{b as a,A as o}from"./index.bf253f1c.js";const c=()=>a(o,{href:"mailto:contact@aktasalper.com",children:"contact@aktasalper.com"});export{c as M};
