9810b0e57bd70773d1a8500dcdf8e4c22f541685
Alper Halil <aktasalper99@gmail.com>
2025-09-16 21:19:05 +0200

    build files

---
+++ dist/assets/ErrorPage.8f43c848.js
@@ -0,0 +1,1 @@
+import{M as s}from"./MailLink.46c4805c.js";import{u as n,a as t,j as a,F as l,b as r,T as c}from"./index.2b253c30.js";function u(){const{t:o}=n("content"),e=t();return console.error(e),a(l,{direction:"column",style:{color:"#fff"},gap:8,children:[r(c,{children:o("error.fallback-element")}),r(s,{})]})}export{u as ErrorPage};
