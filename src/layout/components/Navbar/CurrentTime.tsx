1ec5f99e4b8c39ca347eaae0e84e4fe4a440ae15
Alper Halil <aktasalper99@gmail.com>
2025-09-16 21:18:53 +0200

    update status bar clock handling

--- src/layout/components/Navbar/CurrentTime.tsx
+++ src/layout/components/Navbar/CurrentTime.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-export const CurrentTime = () => (
-	<span>{new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</span>
-);
+export const CurrentTime = () => {
+	const getTime = () => new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+	const [time, setTime] = useState(getTime);
+
+	useEffect(() => {
+		const now = new Date();
+		const msToNextMinute = (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
+		let intervalId: NodeJS.Timeout;
+		const timeoutId = setTimeout(() => {
+			setTime(getTime());
+			intervalId = setInterval(() => {
+				setTime(getTime());
+			}, 60 * 1000);
+		}, msToNextMinute);
+		return () => {
+			clearTimeout(timeoutId);
+			if (intervalId) clearInterval(intervalId);
+		};
+	}, []);
+
+	return <span>{time}</span>;
+};
