c01b82aaac89e20e06c3c39708c643507a48b4df
Alper Halil <aktasalper99@gmail.com>
2025-09-17 22:50:01 +0200

    update blinking avatar

--- src/components/BlinkingEyesAvatar.tsx
+++ src/components/BlinkingEyesAvatar.tsx
@@ -1,27 +1,7 @@
-import { useEffect } from "react";
 import { Avatar } from "react95";
 
-import useCounter from "@/hooks/useCounter";
+import styles from "./BlinkingEyesAvatar.module.scss";
 
-import PixelatedIMG from "@/assets/pixelated.png";
-import PixelatedIMGEyesClosed from "@/assets/pixelated_c.png";
-
-export function BlinkingEyesAvatar({ avatarSize = 80 }: { avatarSize?: number }) {
-	// TODO: switch to GIF or webp?
-	const { count, increment } = useCounter(0);
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			increment();
-		}, 150);
-		return () => clearInterval(interval);
-	}, []);
-
-	return (
-		<Avatar
-			size={avatarSize}
-			src={count % 10 ? PixelatedIMG : PixelatedIMGEyesClosed}
-			style={{ pointerEvents: "none" }}
-		/>
-	);
-}
+export const BlinkingEyesAvatar = ({ avatarSize = 80 }: { avatarSize?: number }) => (
+	<Avatar size={avatarSize} src='/misc/blinking.webp' className={styles.avatar} />
+);
