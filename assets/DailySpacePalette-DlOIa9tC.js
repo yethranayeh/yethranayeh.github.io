0bbc8f156a164c090260dbee5fd1c6ed19accc94
Alper Halil <aktasalper99@gmail.com>
2026-04-18 19:49:26 +0200

    build

--- assets/DailySpacePalette-DlOIa9tC.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as t,T as p}from"./index-Dwu_cKbM.js";import{P as s}from"./ProjectLayout-C5I3evd2.js";import"./github-DkSBEjY0.js";function c(){const{t:e}=a("content");return t.jsx(s,{website:"https://daily-space-palette.vercel.app/",repo:"https://github.com/yethranayeh/daily-space-palette",children:t.jsx(p,{children:e("daily-space-palette")})})}export{c as DailySpacePalette};
