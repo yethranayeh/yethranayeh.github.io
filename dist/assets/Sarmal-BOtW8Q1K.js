f89b5790673acaf18135808336479531162cbce0
Alper Halil <aktasalper99@gmail.com>
2026-05-20 19:42:23 +0200

    build files

--- dist/assets/Sarmal-BOtW8Q1K.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as t,T as s}from"./index-D9kl8esZ.js";import{P as o}from"./ProjectLayout-Hcjb2Mdr.js";import"./github-KyUv44vp.js";function i(){const{t:r}=a("content");return t.jsx(o,{website:"https://sarmal.art",repo:"https://github.com/yethranayeh/sarmal",children:t.jsx(s,{children:r("sarmal")})})}export{i as Sarmal};
