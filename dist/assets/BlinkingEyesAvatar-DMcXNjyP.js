a6db4bc2187db237325a6832dd466de22da47368
Alper Halil <aktasalper99@gmail.com>
2026-06-03 20:55:36 +0200

    build files

--- dist/assets/BlinkingEyesAvatar-DMcXNjyP.js
+++
@@ -1,1 +0,0 @@
-import{v as s,c as t}from"./index-CgTvJPvd.js";const r="_avatar_14pul_59",i={avatar:r},c=({avatarSize:a=80})=>s.jsx(t,{size:a,src:"/misc/blinking.webp",className:i.avatar});export{c as B};
